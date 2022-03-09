224088e45ac0491a61b8f0487b6fb24c20121d0c
EastHill <aquabuz@gmail.com>
2022-03-09 22:26:16 +0900

    work-fin

--- src/components/LayerPopUp.vue
+++ src/components/LayerPopUp.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- layer popup -->
   <div
-    class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/4 max-w-md rounded-md bg-white shadow-lg overflow-hidden z-20"
+    class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/4 max-w-md rounded-md bg-white shadow-lg z-20"
   >
     <!-- title -->
     <div
@@ -26,8 +26,9 @@
         </label>
         <input
           id="datepicker-date"
-          type="text"
-          class="p-1 mx-2 flex-1 border border-slate-900 rounded-md"
+          class="p-1 mx-2 border border-slate-900 rounded-md"
+          placeholder="Select Date"
+          type="date"
         />
       </div>
       <div class="flex items-center">
@@ -36,8 +37,10 @@
         </label>
         <input
           id="datepicker-time-start"
-          type="text"
-          class="p-1 mx-2 flex-1 border border-slate-900 rounded-md"
+          class="p-1 mx-2 border border-slate-900 rounded-md"
+          type="time"
+          min="00:00"
+          max="24:00"
         />
       </div>
       <div class="flex items-center">
@@ -46,17 +49,60 @@
         </label>
         <input
           id="datepicker-time-end"
-          type="text"
-          class="p-1 mx-2 flex-1 border border-slate-900 rounded-md"
+          class="p-1 mx-2 border border-slate-900 rounded-md"
+          type="time"
+          min="00:00"
+          max="24:00"
         />
       </div>
       <div class="flex items-center">
         <label for="course" class="w-1/5 text-sm">
           코스
         </label>
-        <select id="course">
-          전체 선택
-        </select>
+        <div class="relative flex-1">
+          <!-- layer select -->
+          <div
+            id="course"
+            class="p-1 mx-2 flex-1 border border-slate-900 rounded-md"
+            @click="showCourse"
+          >
+            <span>선택</span>
+          </div>
+          <!-- layer -->
+          <div
+            v-if="isShow"
+            class="absolute p-4 flex flex-col top-0 left-0 w-full border border-slate-900 rounded-md bg-white"
+          >
+            <!-- list -->
+            <ul>
+              <li
+                v-for="(item, index) in items"
+                :key="index"
+                class="flex items-center"
+              >
+                <input type="checkbox" :id="`check-${item}`" class="mr-3" />
+                <label :for="`check-${item}`" class="flex">{{ item }}</label>
+              </li>
+            </ul>
+            <!-- button -->
+            <!-- left button -->
+            <div class="mt-5 gap-2 flex">
+              <button
+                type="button"
+                class="py-1 px-4 font-bold text-white rounded-sm bg-blue-500 flex-2"
+                @click="isShow = !isShow"
+              >
+                확인
+              </button>
+              <button
+                type="button"
+                class="py-1 px-4 font-bold text-white rounded-sm bg-gray-400 flex-1"
+              >
+                취소
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
       <!-- save -->
       <button
@@ -78,17 +124,33 @@ import { XIcon } from "@heroicons/vue/solid";
 export default {
   name: "LayerPopUp",
   emits: ["hideLayer"],
-  components: { XIcon },
+  components: {
+    XIcon,
+  },
   setup(props, { emit }) {
-    const state = reactive({});
+    const state = reactive({
+      items: [
+        "Select All",
+        "레이크 > 파인",
+        "필드 > 마운틴",
+        "마운틴 > 밸리",
+        "마운틴 > 마운틴",
+      ],
+      isShow: false,
+    });
 
     const hideLayer = () => {
       emit("hideLayer", false);
     };
 
+    const showCourse = () => {
+      state.isShow = !state.isShow;
+    };
+
     return {
       ...toRefs(state),
       hideLayer,
+      showCourse,
     };
   },
 };
